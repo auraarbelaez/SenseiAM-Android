4eceaf8258e06a6c3bbdcfe641e6354210e86e17
SantiagoHB <santiago.hernandezl@outlook.com>
2024-05-13 13:52:54 -0500

    carga archivos

---
+++ app/src/main/java/com/example/senseiam/MainActivity.kt
@@ -0,0 +1,14 @@
+import android.os.Bundle
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myWebView: WebView = findViewById(R.id.webview)
+        myWebView.loadUrl("file:///android_asset/login.html")
+    }
+}
